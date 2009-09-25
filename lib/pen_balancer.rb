05f5e1ebea52fabaa2800530089c4787c969cd6f
Jannis Hermanns <jannis@moviepilot.de>
2009-09-25 17:16:51 +0200

    Added all commands from man penctl

--- lib/pen_balancer.rb
+++ lib/pen_balancer.rb
@@ -1,19 +1,29 @@
 class PenBalancer
   
+  BOOLEAN_ATTRIBS = [:ascii=, :block=, :conn_max=, :control=, :control_acl=, :delayed_forward=, :hash=, :http=, :roundrobin=, :stubborn=, :weight=]
+  GETTERS_SETTERS = [:blacklist, :client_acl, :control_acl, :debug, :log, :tracking, :timeout, :web_stats]
+  GETTERS         = [:clients_max, :conn_max, :control, :listen, :recent, :status]
+  COMMANDS        = [:exit, :include, :write]
   #
   #  Creates a new PenBalancer instance: If you have launched a local pen
   #  instance with 8080 as the control port, you should use PenBalancer 
   #  like this: PenBalancer.new 'localhost:8080'
-  #  
   def initialize( address )
     @pen = address
   end
 
   #
+  #  Returns an array of servers pen currently knows
+  def servers
+    list = []
+    execute_penctl("servers").each{ |l| list << parse_server_line(l) }
+    list
+  end
+
+  #
   #  Takes a line of the output of penctl's 'servers' command and turns it into
   #  a hash with the keys :slot, :addr:, :port, :conn, :max, :hard, :sx, :rx
-  #
-  def self.parse_line( line )
+  def self.parse_server_line( line )
     keys = %w{ slot addr port conn max hard sx rx}
     hash = Hash[*("slot #{line}".split)]
     server = {}
@@ -21,8 +31,40 @@ class PenBalancer
     return server
   end
 
-  def execute( cmd )
+  #
+  #  Calls the penctl binary and issues a commend
+  def execute_penctl( cmd )
     cmd = "penctl #{@pen} #{cmd}"
-    IO.popen(cmd).readlines
+    IO.popen(cmd).readlines.map(&:chomp)
   end
+  
+  #
+  #  penctl has the following three patterns:
+  #    1) booleans (no getters): penctl localhost:8080 no ascii
+  #                              penctl localhost:8080 ascii
+  #    2) getters/setters:       penctl localhost:8080 debug 3
+  #                              penctl localhost:8080 debug
+  #    3) commands:              penctl localhost:8080 exit
+  #  The first two are turned into regular getters and setters and
+  #  the last into methods.
+  def method_missing(method, *args)
+    return set_boolean_attribute(method, args[0]) if BOOLEAN_ATTRIBS.include? method
+    return get_set_attribute(method, args[0])     if GETTERS_SETTERS.include? method.to_s.chomp('=').to_sym
+    return execute_penctl(method.to_s)==[]        if COMMANDS.include? method
+    raise 
+  end
+  
+  def set_boolean_attribute(attribute, value)
+    cmd = attribute.to_s.chomp '='
+    cmd = value ? cmd : "no " + cmd
+    return execute_penctl(cmd) == [0]
+  end
+  
+  def get_set_attribute(attribute, value)
+    value ||= 0
+    cmd   = attribute.to_s.chomp '='
+    value = attribute.to_s['='] ? " #{value}" : ''
+    return execute_penctl("#{cmd}#{value}".chomp)[0].to_i # All attributes you can read from penctl are integers
+  end
+  
 end
--- spec/lib/pen_balancer_spec.rb
+++ spec/lib/pen_balancer_spec.rb
@@ -4,14 +4,58 @@ require 'lib/pen_balancer'
 describe PenBalancer do
 
   describe "instance methods" do
-    it "" do
-      
+    
+    before(:each) do
+      @pen = PenBalancer.new '127.0.0.1:12000'
     end
+    
+    it ":servers should return an array of hashes with the servers pen currently knows" do
+      servers_reply = ["0 addr 127.0.0.1 port 12101 conn 0 max 0 hard 0 sx 1054463671 rx 2586728338",
+                       "1 addr 127.0.0.1 port 12501 conn 1 max 0 hard 0 sx 1103014051 rx 2688785671"]
+      @pen.should_receive(:execute_penctl).with("servers").and_return servers_reply
+      @pen.should_receive(:parse_server_line).twice.and_return "hash"
+      
+      result = @pen.servers
+      result.should have(2).items
+      result[0].should == "hash"
+      result[1].should == "hash"
+    end
+    
+    it "should set boolean variables (e.g. http)" do
+      @pen.should_receive(:execute_penctl).with('http')
+      @pen.http = true
+    end
+    
+    it "should set boolean variables (e.g. no http)" do
+      @pen.should_receive(:execute_penctl).with('no http')
+      @pen.http = false
+    end
+    
+    it "should set other variables (e.g. debug 5)" do
+      @pen.should_receive(:execute_penctl).with('debug 5').and_return ["5"]
+      @pen.debug = 5
+    end
+    
+    it "should get other variables (e.g. debug)" do
+      @pen.should_receive(:execute_penctl).with('debug').and_return ["5"]
+      @pen.debug.should == 5
+    end
+    
+    it "should issue commands (e.g. exit)" do
+      @pen.should_receive(:execute_penctl).with('exit').and_return []
+      @pen.exit.should be_true
+    end
+    
+    it "should return false when a command could not be issued" do
+      @pen.should_receive(:execute_penctl).with('exit').and_return ["Exit is not enabled; restart with -X flag"]
+      @pen.exit.should be_false
+    end
+    
   end
   
   describe "class methods" do
     
-    it ":parse_line should turn penctl servers output into a hash" do
+    it ":parse_server_line should turn penctl servers output into a hash" do
       line = "1 addr 127.0.0.1 port 12501 conn 2709 max 2212 hard 2 sx 1092895943 rx 2664422154"
       expected = { :slot => 1,
                    :addr => '127.0.0.1',
@@ -21,13 +65,13 @@ describe PenBalancer do
                    :hard => 2,
                    :sx   => 1092895943,
                    :rx   => 2664422154 }
-      PenBalancer.parse_line(line).should == expected
+      PenBalancer.parse_server_line(line).should == expected
     end
     
     it ":execute should call the penctl binary and contact the right pen" do
       pen = PenBalancer.new '127.0.0.1:12000'
       IO.should_receive(:popen).with("penctl 127.0.0.1:12000 foo").and_return mock(:readlines => ["something"])
-      pen.execute("foo").should == ["something"]
+      pen.execute_penctl("foo").should == ["something"]
     end
     
   end

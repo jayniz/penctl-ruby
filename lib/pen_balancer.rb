ef27d4e2b34fc665daa9fbf7dfb049b984f1bbd5
Jannis Hermanns <jannis@moviepilot.de>
2009-09-25 15:02:55 +0200

    Added method to issue penctl commands

--- lib/pen_balancer.rb
+++ lib/pen_balancer.rb
@@ -1,12 +1,18 @@
 class PenBalancer
   
-  def initialize( options )
-    unknown_keys = options.keys - [:host, :port].flatten
-    raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") unless unknown_keys.empty?
+  #
+  #  Creates a new PenBalancer instance: If you have launched a local pen
+  #  instance with 8080 as the control port, you should use PenBalancer 
+  #  like this: PenBalancer.new 'localhost:8080'
+  #  
+  def initialize( address )
+    @pen = address
   end
-  
-  protected
-  
+
+  #
+  #  Takes a line of the output of penctl's 'servers' command and turns it into
+  #  a hash with the keys :slot, :addr:, :port, :conn, :max, :hard, :sx, :rx
+  #
   def self.parse_line( line )
     keys = %w{ slot addr port conn max hard sx rx}
     hash = Hash[*("slot #{line}".split)]
@@ -14,8 +20,9 @@ class PenBalancer
     keys.each { |k| server[k.to_sym] = (k == "addr") ? hash[k] : hash[k].to_i  }
     return server
   end
-  
-end
 
-__END__
-line = "1 addr 127.0.0.1 port 12501 conn 2709 max 2212 hard 0 sx 1092895943 rx 2664422154"
+  def execute( cmd )
+    cmd = "penctl #{@pen} #{cmd}"
+    IO.popen(cmd).readlines
+  end
+end
--- spec/lib/pen_balancer_spec.rb
+++ spec/lib/pen_balancer_spec.rb
@@ -2,28 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'lib/pen_balancer'
 
 describe PenBalancer do
-  
-  describe ":initialize" do
-    it "should accept a hash with :host and :port set" do
-      lambda{
-        PenBalancer.new( :host => '127.0.0.1', :port => '123' )
-      }.should_not raise_error
-    end
-  
-    it "should not accept a hash with missing :server" do
-      lambda{
-        PenBalancer.new( :server => '127.0.0.1' )
-      }.should raise_error(ArgumentError)
+
+  describe "instance methods" do
+    it "" do
+      
     end
   end
   
-  describe ":servers" do
+  describe "class methods" do
     
-  end
-  
-  describe "penctl output parsing" do
-    
-    it "should be able to parse a line" do
+    it ":parse_line should turn penctl servers output into a hash" do
       line = "1 addr 127.0.0.1 port 12501 conn 2709 max 2212 hard 2 sx 1092895943 rx 2664422154"
       expected = { :slot => 1,
                    :addr => '127.0.0.1',
@@ -34,6 +22,12 @@ describe PenBalancer do
                    :sx   => 1092895943,
                    :rx   => 2664422154 }
       PenBalancer.parse_line(line).should == expected
+    end
+    
+    it ":execute should call the penctl binary and contact the right pen" do
+      pen = PenBalancer.new '127.0.0.1:12000'
+      IO.should_receive(:popen).with("penctl 127.0.0.1:12000 foo").and_return mock(:readlines => ["something"])
+      pen.execute("foo").should == ["something"]
     end
     
   end
